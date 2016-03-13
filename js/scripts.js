f4d016d903fbbcdc25c9bb7f77a192248c0f24f0
Manasa <Manasa>
2016-03-13 10:15:18 -0400

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function()
+{
+	$('.slider').slider({full_width: true});
+	$('.parallax').parallax();
+	$('.materialboxed').materialbox();
+	$('.scrollspy').scrollSpy();
+	$(".dropdown-button").dropdown({click:false});
+	$('.fixed-action-btn').openFAB();
+	$('.fixed-action-btn').closeFAB();
+});
